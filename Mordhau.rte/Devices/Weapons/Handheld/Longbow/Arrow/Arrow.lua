fca3e0e2477051422afa1b4727c580fa8f71cece
filipex2000 <filipex570@gmail.com>
2021-06-04 23:27:02 +0200

    bow and crossbow update

---
+++ Mordhau.rte/Devices/Weapons/Handheld/Longbow/Arrow/Arrow.lua
@@ -0,0 +1,288 @@
+
+function Create(self)
+	--[[
+	local trail = CreateMOPixel("Bow Arrow Trail"); -- Add the coolest thing, custom trail TM
+	trail.Team = self.Team -- You can remove it if you wish, the trail used to damage the target
+	trail.Pos = self.Pos - Vector(self.Vel.X,self.Vel.Y):SetMagnitude(self.IndividualRadius * 0.9);
+	trail.Vel = self.Vel;
+	MovableMan:AddParticle(trail);
+	
+	self.trailID = trail.UniqueID]]
+	
+	self.soundImpact = CreateSoundContainer("Longbow Arrow Impact", "Mordhau.rte");
+	
+	self.soundWiggle = CreateSoundContainer("Longbow Arrow Wiggle", "Mordhau.rte");
+	self.soundWigglePlay = true
+	
+	self.soundBounce = CreateSoundContainer("Longbow Arrow Bounce", "Mordhau.rte");
+	self.soundBouncePlay = true
+	
+	self.soundFlyby = CreateSoundContainer("Longbow Arrow Flyby", "Mordhau.rte");
+	self.soundFlyby.Pitch = 1.1
+	self.soundFlyby:Play(self.Pos)
+	
+	self.Frame = math.random(0, self.FrameCount - 1);
+	
+	self.spawnPoof = true;
+	
+	self.stuck = false;
+	
+	self.stickMO = nil;
+	self.stickVecX = 0;
+	self.stickVecY = 0;
+	self.stickRot = 0;
+	self.stickDeepness = RangeRand(0.1, 1);
+	self.stickWiggle = 0;
+	
+	self.woundLimitChange = false; -- If changed the wound limit, revert it to default if despawning
+	
+	self.Vel = self.Vel + Vector(0, -2); -- Don't ask, the flechette has fucked up trajectory
+	
+	self.decayTimer = nil;
+	self.decayTime = RangeRand(1000,4000);--3000;
+	self.decayGib = math.random(1,5) >= 2;
+	
+	self.deleteTimer = Timer()
+	
+	self.phase = 0;
+end
+
+function Update(self)
+	-- Avoid stupid situations
+	if self.deleteTimer:IsPastSimMS(10000) then
+		self:GibThis();
+	end
+	
+	self.soundFlyby.Pos = self.Pos
+	
+	--- Areodynamics
+	if not self.stuck then
+		local v = self.Vel.Magnitude;
+		-- Balance
+		
+		-- FROTATE
+		-- attention: magic below!
+		local min_value = -math.pi;
+		local max_value = math.pi;
+		local value = self.RotAngle - self.Vel.AbsRadAngle;
+		local result;
+		
+		local range = max_value - min_value;
+		if range <= 0 then
+			result = min_value;
+		else
+			local ret = (value - min_value) % range;
+			if ret < 0 then ret = ret + range end
+			result = ret + min_value;
+		end
+		
+		local a = 5 * v / 60;
+		local b = 10 * v / 60;
+		
+		self.RotAngle = self.RotAngle + result * TimerMan.DeltaTimeSecs * a;
+		self.AngularVel = (self.AngularVel) / (1 + TimerMan.DeltaTimeSecs * b)
+		--self.GlobalAccScalar = (1 + (1/math.sqrt(1 + math.abs(self.Vel.X)/10))) / 2; -- thank your 4zk!
+	end
+	
+	--self.ToDelete = false; -- no, stop, no delete plz
+	
+	--[[
+	if self.trailID then
+		local MO = MovableMan:FindObjectByUniqueID(self.trailID)
+		if MO then
+			MO = ToMOPixel(MO)
+			if self.stuck then
+				MO.ToDelete = true;
+			else
+				MO.Pos = self.Pos - Vector(self.Vel.X,self.Vel.Y):SetMagnitude(self.IndividualRadius * 0.9);
+				MO.Vel = self.Vel;
+				MO.ToDelete = false;
+				MO.ToSettle = false;
+			end
+		end
+	end]]
+	
+	if self.phase == 0 and self.Vel.Magnitude > 13 then -- Raycast, stick to things
+		local rayOrigin = self.Pos
+		local rayVec = Vector(self.Vel.X,self.Vel.Y):SetMagnitude(self.Vel.Magnitude * rte.PxTravelledPerFrame + self.IndividualRadius);
+		local moCheck = SceneMan:CastMORay(rayOrigin, rayVec, self.ID, self.Team, 0, false, 2); -- Raycast
+		if moCheck ~= rte.NoMOID then
+			local rayHitPos = SceneMan:GetLastRayHitPos()
+			local MO = MovableMan:GetMOFromID(moCheck)
+			if IsMOSRotating(MO) then
+				self.stickMO = ToMOSRotating(MO);
+				local stickVec = SceneMan:ShortestDistance(self.stickMO.Pos,rayHitPos,SceneMan.SceneWrapsX):RadRotate(-self.stickMO.RotAngle);
+				self.stickVecX = stickVec.X;
+				self.stickVecY = stickVec.Y;
+				self.stickRot = self.RotAngle - self.stickMO.RotAngle;
+				self.stickWiggle = 2;
+				
+				self.soundImpact:Play(self.Pos)
+				self.soundFlyby:Stop(-1)
+				
+				-- Damage, create a pixel that makes a hole
+				for i = 0, 2 do
+					local pixel = CreateMOPixel("Longbow Arrow Damage", "Mordhau.rte");
+					pixel.Vel = self.Vel;
+					pixel.Pos = self.Pos - Vector(self.Vel.X,self.Vel.Y):SetMagnitude(self.IndividualRadius * 0.9);
+					pixel.Team = self.Team;
+					pixel.IgnoresTeamHits = true;
+					pixel.WoundDamageMultiplier = 1.1 + pixel.WoundDamageMultiplier * self.Vel.Magnitude / 100;--1.53;
+					MovableMan:AddParticle(pixel);
+				end
+				
+				-- Get the material and set damage multiplier
+				local material = self.stickMO.Material.PresetName;
+				
+				
+				-- Add extra effects based on the material
+				if string.find(material,"Flesh") then
+					local blood = CreateMOSParticle("Blood Spray Particle");
+					blood.Pos = rayHitPos - Vector(self.Vel.X,self.Vel.Y) * rte.PxTravelledPerFrame * 0.1;
+					blood.Vel = Vector(self.Vel.X,self.Vel.Y):RadRotate(math.pi * RangeRand(-0.1, 0.1)) * RangeRand(0.05, 0.1) * -1.0;
+					MovableMan:AddParticle(blood);
+				else
+					for i = 1, 3 do
+						local bloofm = CreateMOSParticle("Tiny Smoke Trail 1");
+						bloofm.Pos = rayHitPos - Vector(self.Vel.X,self.Vel.Y) * rte.PxTravelledPerFrame * 0.1;
+						bloofm.Vel = Vector(self.Vel.X,self.Vel.Y):RadRotate(math.pi * RangeRand(-0.2, 0.2)) * RangeRand(0.1, 0.4) * -1.0;
+						MovableMan:AddParticle(bloofm);
+					end
+				end
+				
+				local effect = CreateMOSRotating("Longbow Arrow Hit Effect", "Mordhau.rte");
+				if effect then
+					effect.Pos = rayHitPos
+					MovableMan:AddParticle(effect);
+					effect:GibThis();
+				end
+				
+				
+				self.PinStrength = 1000;
+				self.Vel = Vector()
+				self.AngularVel = 0;
+				--[[
+				if self.trailID then
+					local MO = MovableMan:FindObjectByUniqueID(self.trailID)
+					if MO then
+						MO.ToDelete = true
+						self.trailID = nil
+					end
+				end]]
+				
+				--[[
+				--(rayHitPos - self.stickMO.Pos)
+				local impulseMO = self.stickMO
+				if impulseMO.RootID ~= rte.NoMOID then
+					local root = ToMOSRotating(MovableMan:GetMOFromID(impulseMO.RootID))
+					if not IsAttachable(root) then
+						impulseMO = root
+					end
+				end
+				self.stickMO:AddImpulseForce(Vector(self.Vel.X, self.Vel.Y):SetMagnitude(100 / self.stickMO.Mass * 2), Vector())
+				]]
+				
+				self.HitsMOs = false;
+				
+				self.stuck = true
+				self.phase = 1
+			end
+			
+			self.decayTimer = Timer();
+		else
+			local terrCheck = SceneMan:CastStrengthSumRay(rayOrigin, rayOrigin + rayVec, 2, 0); -- Raycast
+			if terrCheck > 5 then
+				local rayHitPos = SceneMan:GetLastRayHitPos()
+				self.Pos = rayHitPos - Vector(self.IndividualRadius * self.stickDeepness, 0):RadRotate(self.RotAngle);
+				
+				self.PinStrength = 1000;
+				self.Vel = Vector()
+				self.AngularVel = 0;
+				
+				self.soundFlyby:Stop(-1)
+				
+				self.HitsMOs = false;
+				--[[
+				if self.trailID then
+					local MO = MovableMan:FindObjectByUniqueID(self.trailID)
+					if MO then
+						MO.ToDelete = true
+						self.trailID = nil
+					end
+				end
+				]]
+		
+				local effect = CreateMOSRotating("Longbow Arrow Hit Effect", "Mordhau.rte");
+				if effect then
+					effect.Pos = self.Pos + SceneMan:ShortestDistance(self.Pos,rayHitPos,SceneMan.SceneWrapsX) * 0.5
+					MovableMan:AddParticle(effect);
+					effect:GibThis();
+				end
+				
+				self.stuck = true
+				self.phase = 2
+				
+				self.soundImpact:Play(self.Pos)
+				
+				self.decayTime = self.decayTime * 0.5;
+				
+				self.decayTimer = Timer()
+			end
+		end
+		
+		--PrimitiveMan:DrawLinePrimitive(rayOrigin, rayOrigin + rayVec, 5);
+		
+	elseif self.phase == 1 then -- Stuck into MO
+		if self.stickMO.ID ~= rte.NoMOID and not self.decayTimer:IsPastSimMS(self.decayTime) and self.PinStrength > 10 then
+			--self.Pos = self.stickMO.Pos + Vector(self.stickVec.X, self.stickVec.Y):RadRotate(self.stickMO.RotAngle)
+			self.RotAngle = self.stickMO.RotAngle + self.stickRot + math.sin(self.stickWiggle * math.pi * 4.0) * self.stickWiggle * 0.3 + math.sin(self.stickWiggle * math.pi * 8.0) * 0.1
+			self.Pos = self.stickMO.Pos + Vector(self.stickVecX, self.stickVecY):RadRotate(self.stickMO.RotAngle) - Vector(self.IndividualRadius * self.stickDeepness, 0):RadRotate(self.RotAngle);
+			self.PinStrength = 1000;
+			self.AngularVel = 0;
+			
+			--self.stickWiggle = (self.stickWiggle) / (1 + TimerMan.DeltaTimeSecs * 5)
+			self.stickWiggle = math.max(0, self.stickWiggle - TimerMan.DeltaTimeSecs * 4)
+			if self.soundWigglePlay then
+				self.soundWiggle:Play(self.Pos)
+				self.soundWigglePlay = false
+			end
+		else
+			
+			self.PinStrength = 0;
+			
+			self.stickMO = nil;
+			self.stuck = false
+			self.phase = 3
+		end
+		
+	elseif self.phase == 2 then -- Stuck into terrain
+		self.PinStrength = 1000;
+		self.AngularVel = 0;
+		
+		if self.decayTimer:IsPastSimMS(self.decayTime) then
+			self.ToSettle = true;
+		end
+	elseif self.phase == 3 then -- Fell from MO
+		
+	end
+	
+end
+
+function OnCollideWithTerrain(self, terrainID) -- delet
+	if self.phase == 3 and self.decayGib then
+		self:GibThis();
+		--self.ToDelete = true;
+	else
+		self.ToSettle = true;
+		self.phase = 3
+		
+		if self.soundBouncePlay and self.Vel.Magnitude > 5 then
+			self.soundBounce:Play(self.Pos)
+			self.soundBouncePlay = false
+		end
+	end
+end
+
+function Destroy(self)
+	self.soundFlyby:Stop(-1)
+end
---
+++ Mordhau.rte/Devices/Weapons/Handheld/Longbow/Longbow.lua
@@ -0,0 +1,260 @@
+
+
+function Create(self)
+	self.parentSet = false;
+	
+	self.soundFire = CreateSoundContainer("Longbow Fire", "Mordhau.rte");
+	self.soundDraw = CreateSoundContainer("Longbow Draw", "Mordhau.rte");
+	self.soundDrop = CreateSoundContainer("Longbow Arrow Bounce", "Mordhau.rte");
+	
+	
+	self.pastReloadTimer = Timer()
+	
+	self.chargeTimer = Timer()
+	self.chargeTime = 2600
+	self.lastChargeFactor = 0
+	self.charging = false
+	
+	self.arrowVelocityMax = 65
+	self.arrowVelocityMin = 15
+	
+	self.shoot = false
+	
+	self.originalStanceOffset = Vector(math.abs(self.StanceOffset.X), self.StanceOffset.Y)
+	
+	self.originalJointOffset = Vector(self.JointOffset.X, self.JointOffset.Y)
+	
+	self.lastAge = self.Age + 0
+	
+	self.laserTimer = Timer();
+	self.guideTable = {};
+
+	self.projectileVel = 100;
+	if self.Magazine ~= null and self.Magazine.RoundCount ~= 0 then
+		self.projectileVel = self.Magazine.NextRound.FireVel;
+	end
+
+	self.maxTrajectoryPars = 120;
+end
+
+function Update(self)
+	if self.ID == self.RootID then
+		self.parent = nil;
+		self.parentSet = false;
+	elseif self.parentSet == false then
+		local actor = MovableMan:GetMOFromID(self.RootID);
+		if actor and IsAHuman(actor) then
+			self.parent = ToAHuman(actor);
+			self.parentSet = true;
+		end
+	end
+	
+	local sharpaiming = false
+	local stanceOffset = Vector()
+	
+	-- Check if switched weapons/hide in the inventory, etc.
+	if self.Age > (self.lastAge + TimerMan.DeltaTimeSecs * 2000) then
+		--
+		self.chargeTimer:Reset()
+		self.charging = false
+		self.shoot = false
+		self.lastChargeFactor = 0
+	end
+	self.lastAge = self.Age + 0
+	
+	if self.parent then
+		sharpaiming = self.parent:GetController():IsState(Controller.AIM_SHARP) == true and self.parent:GetController():IsState(Controller.MOVE_LEFT) == false and self.parent:GetController():IsState(Controller.MOVE_RIGHT) == false
+		
+		if self:DoneReloading() then
+			--
+			self.chargeTimer:Reset()
+			self.charging = false
+			self.shoot = false
+			
+			self.pastReloadTimer:Reset()
+			
+			self.JointOffset = self.originalJointOffset
+			
+			self.Frame = 0
+		elseif self:IsReloading() then
+			--
+			self.chargeTimer:Reset()
+			self.charging = false
+			self.shoot = false
+			
+			self.pastReloadTimer:Reset()
+			
+			--self.JointOffset = self.originalJointOffset
+			self.JointOffset = (self.JointOffset + self.originalJointOffset * TimerMan.DeltaTimeSecs * 15) / (1 + TimerMan.DeltaTimeSecs * 15)
+			
+			self.Frame = 0
+		else
+			local active = self:IsActivated()
+			self:Deactivate()
+			
+			local chargeFactor = math.min(self.chargeTimer.ElapsedSimTimeMS / self.chargeTime, 1)
+			if self.Magazine then
+				self.Magazine.JointOffset.X = chargeFactor * 8 - 5
+				
+				--self.JointOffset = Vector(self.Magazine.JointOffset.X * -0.5 - 6, self.Magazine.JointOffset.Y)
+				
+				self.JointOffset = (self.JointOffset + Vector(self.Magazine.JointOffset.X * -0.5 - 6, self.Magazine.JointOffset.Y) * TimerMan.DeltaTimeSecs * 15) / (1 + TimerMan.DeltaTimeSecs * 15)
+				
+				--self.StanceOffset = Vector((1 - chargeFactor) * 2 + 1, self.originalStanceOffset.Y)
+				--self.StanceOffset = self.originalStanceOffset + Vector(chargeFactor * -5, 0) - Vector(-self.Magazine.JointOffset.X - 8, self.Magazine.JointOffset.Y)
+				
+				stanceOffset = Vector((1 - chargeFactor) * 5 + 2, self.originalStanceOffset.Y)
+				
+				self.Frame = math.floor(chargeFactor * 2 + 0.5)
+				
+				--self.lastChargeFactor = chargeFactor
+			else
+				self.JointOffset = self.originalJointOffset
+				self.Frame = 0
+			end
+			
+			if active and self.Magazine then
+				if not self.charging then
+					self.charging = true
+					self.chargeTimer:Reset()
+					self.soundDraw:Play(self.Pos)
+				end
+			else
+				if self.charging then
+					self.charging = false
+					self.shoot = true
+					self.lastChargeFactor = chargeFactor
+				end
+				self.chargeTimer:Reset()
+			end
+			
+			self.projectileVel = self.arrowVelocityMin + (self.arrowVelocityMax - self.arrowVelocityMin) * chargeFactor
+			
+			if self.shoot then
+				self:Activate()
+			end
+			
+			if self.pastReloadTimer:IsPastSimMS(60) then
+				if self.FiredFrame then
+					
+					--
+					local arrow = CreateMOSRotating("Longbow Arrow", "Mordhau.rte");
+					arrow.Pos = self.Pos + Vector(self.MuzzleOffset.X * self.FlipFactor, self.MuzzleOffset.Y):RadRotate(self.RotAngle + RangeRand(-0.1,0.1));
+					arrow.Vel = self.Vel + Vector(1 * self.FlipFactor,0):RadRotate(self.RotAngle) * (self.arrowVelocityMin + (self.arrowVelocityMax - self.arrowVelocityMin) * self.lastChargeFactor) * RangeRand(0.95, 1.05);
+					arrow.RotAngle = self.RotAngle + (math.pi * (-self.FlipFactor + 1) / 2)
+					
+					arrow.Team = self.parent.Team;
+					arrow.IgnoresTeamHits = true;
+					
+					MovableMan:AddParticle(arrow);
+					
+					self.lastChargeFactor = 0
+					
+					self.soundFire:Play(self.Pos)
+					
+					self.soundDraw:Stop()
+					
+					self.shoot = false
+					self.chargeTimer:Reset()
+				end
+			elseif self.Magazine and self.Magazine.RoundCount < 1 then
+				self.Magazine.RoundCount = 1
+			end
+		end
+		
+
+	end
+	local stringMode = 0
+	
+	if self.Magazine then
+		if self.Magazine.RoundCount < 1 then
+			self.Magazine.Scale = 0
+		else
+			self.Magazine.Scale = 1
+			
+			stringMode = 1
+		end
+	end
+	
+	local stringStartPosA = self.Pos + Vector((-3 - self.Frame) * self.FlipFactor, -14):RadRotate(self.RotAngle)
+	local stringStartPosB = self.Pos + Vector((-3 - self.Frame) * self.FlipFactor, 14):RadRotate(self.RotAngle)
+	local stringMiddlePos = self.Pos + Vector((-3 - self.Frame) * self.FlipFactor, 0):RadRotate(self.RotAngle)
+	local stringColor = 168
+	
+	if stringMode == 1 then
+		local arrowPos = self.Magazine.Pos + Vector(-6 * self.Magazine.FlipFactor, 0):RadRotate(self.Magazine.RotAngle)
+		arrowPos = SceneMan:ShortestDistance(stringMiddlePos,arrowPos,SceneMan.SceneWrapsX)
+		arrowPos:RadRotate(-self.Magazine.RotAngle)
+		arrowPos.X = math.min(arrowPos.X, 0)
+		arrowPos:RadRotate(self.Magazine.RotAngle)
+		arrowPos = arrowPos + stringMiddlePos
+		
+		PrimitiveMan:DrawLinePrimitive(stringStartPosA, stringStartPosA + SceneMan:ShortestDistance(stringStartPosA,arrowPos,SceneMan.SceneWrapsX), stringColor)
+		PrimitiveMan:DrawLinePrimitive(stringStartPosB, stringStartPosB + SceneMan:ShortestDistance(stringStartPosB,arrowPos,SceneMan.SceneWrapsX), stringColor)
+	else
+		PrimitiveMan:DrawLinePrimitive(stringStartPosA, stringStartPosA + SceneMan:ShortestDistance(stringStartPosA,stringStartPosB,SceneMan.SceneWrapsX), stringColor)
+	end
+	
+	if self:IsReloading() or not self.Magazine then
+		self.SharpStanceOffset = Vector(10,4)
+		self.StanceOffset = Vector(10,4)
+	else
+		self.SharpStanceOffset = stanceOffset + Vector(1, -2)
+		self.StanceOffset = stanceOffset
+	end
+	
+	
+	--
+	--[[
+	local actor = self.parent
+	if self.charging and not self:IsReloading() and MovableMan:IsActor(actor) and ToActor(actor):IsPlayerControlled() and ToActor(actor):GetController():IsState(Controller.AIM_SHARP) then
+		if self.laserTimer:IsPastSimMS(25) then
+			self.laserTimer:Reset();
+
+			self.guideTable = {};
+			self.guideTable[1] = Vector(self.MuzzlePos.X,self.MuzzlePos.Y);
+
+			local actor = ToActor(actor);
+			local guideParPos = self.MuzzlePos;
+			local guideParVel = Vector(self.projectileVel * self.FlipFactor,0):RadRotate(self.RotAngle)
+			--local guideParVel = Vector(self.projectileVel,0):RadRotate(actor:GetAimAngle(true));
+			for i = 1, self.maxTrajectoryPars do
+				guideParVel = guideParVel + SceneMan.GlobalAcc * TimerMan.DeltaTimeSecs;
+				guideParPos = guideParPos + guideParVel * rte.PxTravelledPerFrame;
+				-- No need to wrap the seam manually, primitives can handle out-of-scene coordinates correctly
+				--if SceneMan.SceneWrapsX == true then
+				--	if guideParPos.X > SceneMan.SceneWidth then
+				--		guideParPos = Vector(guideParPos.X - SceneMan.SceneWidth,guideParPos.Y);
+				--	elseif guideParPos.X < 0 then
+				--		guideParPos = Vector(SceneMan.SceneWidth + guideParPos.X,guideParPos.Y);
+				--	end
+				--end
+				if SceneMan:GetTerrMatter(guideParPos.X,guideParPos.Y) == 0 then
+					self.guideTable[#self.guideTable+1] = guideParPos;
+				else
+					local hitPos = Vector(self.guideTable[#self.guideTable].X,self.guideTable[#self.guideTable].Y);
+					SceneMan:CastStrengthRay(self.guideTable[#self.guideTable],SceneMan:ShortestDistance(self.guideTable[#self.guideTable],guideParPos,false),0,hitPos,3,0,false);
+					self.guideTable[#self.guideTable+1] = hitPos;
+					break;
+				end
+			end
+		end
+	else
+		self.guideTable = {};
+	end
+
+	if #self.guideTable > 1 then
+		for i = 1, #self.guideTable do
+			PrimitiveMan:DrawLinePrimitive(self.guideTable[i],self.guideTable[i],120);
+		end
+		--PrimitiveMan:DrawCirclePrimitive(self.guideTable[#self.guideTable],4,120);
+	end
+	]]
+end 
+
+function OnCollideWithTerrain(self, terrainID) -- delet
+	if self.Magazine then
+		self.Magazine.JointStrength = -5
+		self.soundDrop:Play(self.Pos)
+	end
+end
