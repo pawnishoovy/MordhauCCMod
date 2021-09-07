fc077cd4added1ace9459e9346051c6e2cd91e6b
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2021-09-07 21:25:06 +0300

    better wind

--- Mordhau.rte/Actors/Shared/Scripts/MovementWind.lua
+++ Mordhau.rte/Actors/Shared/Scripts/MovementWind.lua
@@ -5,21 +5,27 @@ function Create(self)
 	self.movementWindSound.Volume = 0;
 	self.movementWindSound:Play(self.Pos);
 	
+	self.volumeTarget = 0;
+	
 end
 
 function Update(self)
 	
 	self.movementWindSound.Pos = self.Pos;
 	if self.Vel.Magnitude > 3 and self:IsPlayerControlled() then
-		if self.movementWindSound.Volume < 0.09 then
+		if self.movementWindSound.Volume < self.volumeTarget then
 			self.movementWindSound.Volume = self.movementWindSound.Volume + 0.045 * TimerMan.DeltaTimeSecs;
-		else
-			self.movementWindSound.Volume = math.min(1, self.Vel.Magnitude/25 - 0.09)
+		elseif self.movementWindSound.Volume > self.volumeTarget then
+			self.movementWindSound.Volume = self.movementWindSound.Volume - 0.8 * TimerMan.DeltaTimeSecs;
+			if self.movementWindSound.Volume < 0 then
+				self.movementWindSound.Volume = 0
+			end
 		end
+		self.volumeTarget = math.min(1, self.Vel.Magnitude/25)
 		self.movementWindSound.Pitch = math.min(0.9, math.max(1, self.Vel.Magnitude/25 - 0.09))
 	else
 		if self.movementWindSound.Volume > 0 then
-			self.movementWindSound.Volume = self.movementWindSound.Volume - 0.25 * TimerMan.DeltaTimeSecs;
+			self.movementWindSound.Volume = self.movementWindSound.Volume - 0.8 * TimerMan.DeltaTimeSecs;
 			if self.movementWindSound.Volume < 0 then
 				self.movementWindSound.Volume = 0
 			end
